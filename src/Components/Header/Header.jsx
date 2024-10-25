204aab857cd97238c47303a50ba2cbc235b8620b
unknown <asadalgalib9@gmail.com>
2024-10-25 17:04:23 +0600

    project deployed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <div>
-            <h1 className='text-center text-4xl'>Header</h1>
+            <h1 className='text-center text-4xl font-bold'>Header</h1>
         </div>
     );
 };
